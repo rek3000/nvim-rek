9d70bd7b6cac94d74e1a486d7d861f61788d3173
rek3000 <showmeeverybody@gmail.com>
2022-10-28 22:19:54 +0700

    fixed javalsp

--- lua/plug-config/nvim-lsp.lua
+++ lua/plug-config/nvim-lsp.lua
@@ -2,10 +2,7 @@ local lsp = require "lspconfig"
 local coq = require "coq"
 
 lsp.clangd.setup(coq.lsp_ensure_capabilities{})
-lsp.jdtls.setup(coq.lsp_ensure_capabilities{cmd = {{ 
-	"jdtls", 
-	"-configuration", "/home/rek/.local/share/nvim/mason/packages/jdtls/config_linux", 
-	"-data", "/home/rek/.cache/jdtls/workspace" 
-	} }})
+lsp.jdtls.setup(coq.lsp_ensure_capabilities{})
+
 require'lspconfig'.jdtls.setup{}
 
