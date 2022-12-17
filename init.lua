83eb89dd808c6a08ad2a910d38ebd55812c45ba1
rek3000 <showmeeverybody@gmail.com>
2022-12-17 16:01:55 +0700

    change dir

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 require('options')
-require('list-plug.load')
 require('keymaps')
 require('plugins')
 
--- lua/list-plug/nvim-lsp.lua
+++
@@ -1,9 +0,0 @@
-local lsp = require "lspconfig"
-local coq = require "coq"
-
-lsp.rust_analyzer.setup(coq.lsp_ensure_capabilities())
-lsp.jdtls.setup(coq.lsp_ensure_capabilities())
-lsp.clangd.setup(coq.lsp_ensure_capabilities())
---lsp.sumneko_lua.setup(coq.lsp_ensure_capabilities())
-lsp.pylsp.setup(coq.lsp_ensure_capabilities())
-
