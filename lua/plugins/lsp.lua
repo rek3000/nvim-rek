b7b46b96cc0d9e76cb1d15e6af83aa153cf2de09
rek3000 <torek3k@gmail.com>
2025-06-25 23:21:53 +0700

    feat: some plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,90 +1,89 @@
 return {
-    {
-        "ms-jpq/coq_nvim",
-        branch = "coq",
-        dependencies = {
-            {
-                "ms-jpq/coq.artifacts",
-                branch = "artifacts",
-            },
-            {
-                "ms-jpq/coq.thirdparty",
-                branch = "3p",
-            }
-        },
-        config = function()
-            require("coq_3p") {
-                { src = "builtin/html" },
-                { src = "builtin/js" },
-                { src = "builtin/syntax" },
-                { src = "builtin/css" },
-                { src = "nvimlua",       short_name = "nLUA", conf_only = true },
-                { src = "bc",            short_name = "MATH", precision = 6 },
-            }
-        end,
+  {
+    "ms-jpq/coq_nvim",
+    branch = "coq",
+    dependencies = {
+      {
+        "ms-jpq/coq.artifacts",
+        branch = "artifacts",
+      },
+      {
+        "ms-jpq/coq.thirdparty",
+        branch = "3p",
+      }
     },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig"
-        },
-        config = function()
-            local mslsp = require("mason-lspconfig")
-            local lsp = require("lspconfig")
+    config = function()
+      require("coq_3p") {
+        { src = "builtin/html" },
+        { src = "builtin/js" },
+        { src = "builtin/syntax" },
+        { src = "builtin/css" },
+        { src = "nvimlua",       short_name = "nLUA", conf_only = true },
+        { src = "bc",            short_name = "MATH", precision = 6 },
+      }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig"
+    },
+    config = function()
+      local mslsp = require("mason-lspconfig")
+      local lsp = require("lspconfig")
 
-            mslsp.setup {
-                automatic_enable = false,
-                ensure_installed = {}, -- Add servers to install here if needed
-            }
+      mslsp.setup {
+        automatic_enable = false,
+        ensure_installed = {},         -- Add servers to install here if needed
+      }
 
-            vim.g.coq_settings = { auto_start = true }
-            local coq = require("coq")
+      vim.g.coq_settings = { auto_start = true }
+      local coq = require("coq")
 
-            --  Correctly set capabilities.  This should be done once, not per server in the loop.
-            local capabilities = coq.lsp_ensure_capabilities()
-            require("mason").setup()
+      --  Correctly set capabilities.  This should be done once, not per server in the loop.
+      local capabilities = coq.lsp_ensure_capabilities()
+      require("mason").setup()
 
-            for _, server_name in pairs(mslsp.get_installed_servers()) do
-                --  Use server_name directly to get the config function.
-                local config = {
-                    capabilities = capabilities, --  Pass the capabilities here
-                }
-                vim.lsp.enable(server_name)
-                lsp[server_name].setup(config)
-            end
+      for _, server_name in pairs(mslsp.get_installed_servers()) do
+        --  Use server_name directly to get the config function.
+        local config = {
+          capabilities = capabilities,           --  Pass the capabilities here
+        }
+        -- vim.lsp.enable(server_name)
+        lsp[server_name].setup(config)
+      end
 
 
-            -- vim.keymap.set('i', '<C-S-K>', vim.g.coq_settings.keymap.eval_snips)
-            -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-            -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-            -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-            -- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-            -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+      -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover)
-            vim.keymap.set('n', '<leader>wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end)
-            vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition)
-            -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references)
-            vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+      vim.keymap.set('n', '<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end)
+      vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition)
+      -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action)
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+      vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
-            vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-                pattern = { "*.hl", "hypr*.conf" },
-                callback = function(event)
-                    print(string.format("starting hyprls for %s", vim.inspect(event)))
-                    vim.lsp.start {
-                        name = "hyprlang",
-                        cmd = { "hyprls" },
-                        root_dir = vim.fn.getcwd(),
-                    }
-                end
-            })
-        end,
-    }
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+        pattern = { "*.hl", "hypr*.conf" },
+        callback = function(event)
+          print(string.format("starting hyprls for %s", vim.inspect(event)))
+          vim.lsp.start {
+            name = "hyprlang",
+            cmd = { "hyprls" },
+            root_dir = vim.fn.getcwd(),
+          }
+        end
+      })
+    end,
+  }
 }
--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -19,5 +19,19 @@ return {
     -- Smear cursor in insert mode.
     -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
     smear_insert_mode = true,
+    -- cursor_color = "#ff8800",
+    -- stiffness = 0.3,                      -- 0.6      [0, 1]
+    -- trailing_stiffness = 0.1,             -- 0.4      [0, 1]
+    -- stiffness_insert_mode = 0.6,          -- 0.4      [0, 1]
+    -- trailing_stiffness_insert_mode = 0.6, -- 0.4      [0, 1]
+    -- distance_stop_animating = 0.5,        -- 0.1      > 0
+    cursor_color = "#b825a4",
+    stiffness = 0.8,
+    trailing_stiffness = 0.5,
+    distance_stop_animating = 0.5,        -- 0.1      > 0
+    trailing_exponent = 3,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
   },
 }
