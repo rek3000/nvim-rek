c7435484aeb93424e090d16b7c18535759ce4d44
rek3000 <torek3k@gmail.com>
2026-03-09 22:50:23 +0700

    udpate

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -99,17 +99,17 @@ return {
       vim.keymap.set('n', 'gr', vim.lsp.buf.references)
       vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
-        pattern = { "*.hl", "hypr*.conf" },
-        callback = function(event)
-          print(string.format("starting hyprls for %s", vim.inspect(event)))
-          vim.lsp.start {
-            name = "hyprlang",
-            cmd = { "hyprls" },
-            root_dir = vim.fn.getcwd(),
-          }
-        end
-      })
+      -- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWinEnter' }, {
+      --   pattern = { "*.hl", "hypr*.conf" },
+      --   callback = function(event)
+      --     print(string.format("starting hyprls for %s", vim.inspect(event)))
+      --     vim.lsp.start {
+      --       name = "hyprlang",
+      --       cmd = { "hyprls" },
+      --       root_dir = vim.fn.getcwd(),
+      --     }
+      --   end
+      -- })
     end,
   }
 }
